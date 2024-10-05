9c2b8a0163bece6483b46e38ce9c30f81c887ab1
robkenhow77 <102028368+robkenhow77@users.noreply.github.com>
2024-10-05 17:40:14 +0100

    Update and rename Task 2b to Task 2b.py

---
+++ Level 1/Challenge 1/Task 2b.py
@@ -0,0 +1,7 @@
+print("What is your name?")
+name = input()
+if name == "Alice" or name == "Bob":
+    print("Hello " + name + "!")
+else:
+    print("Sorry... You're not authorised to be greeted!")
+    
