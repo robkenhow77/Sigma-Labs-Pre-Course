2fb1e95d64972fc91eed2b3a1c8a2948bc7eb588
robkenhow77 <102028368+robkenhow77@users.noreply.github.com>
2024-10-05 19:16:31 +0100

    Update Task 1.py

--- Level 2/Challenge 2/Task 1.py
+++ Level 2/Challenge 2/Task 1.py
@@ -1,1 +1,30 @@
+from random import randint
+
+
+def guess_number():
+    print("Guess a number between 0 and 100: ")
+    try:
+        return int(input())
+    except:
+        print("invalid number")
+        guess_number()
+
+
+random_number = randint(0,100)
+# print("random number " + str(random_number))
+number_of_guesses = 0
+
+while True:
+    guess = guess_number()
+    number_of_guesses += 1
+    if guess > random_number:
+        print("number is lower")
+    if guess < random_number:
+        print("number is higher")
+    if guess == random_number:
+
+        break
+    print("number of guesses: " + str(number_of_guesses))
+
+print("correct, completed in " + str(number_of_guesses) + " guesses")
 
