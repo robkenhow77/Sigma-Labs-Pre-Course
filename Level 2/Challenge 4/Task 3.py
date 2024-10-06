4c6211e59d1e870a56a5875951f0734bbbc0830b
robkenhow77 <102028368+robkenhow77@users.noreply.github.com>
2024-10-06 11:57:05 +0100

    Update Task 3.py

--- Level 2/Challenge 4/Task 3.py
+++ Level 2/Challenge 4/Task 3.py
@@ -1,1 +1,17 @@
+animals = [
+    {"name": "Fluffy", "type": "dog"},
+    {"name": "Parsley", "type": "dog"},
+    {"name": "Ginger", "type": "cat"},
+    {"name": "Biscuit", "type": "cat"},
+    {"name": "Poppy", "type": "cow"}
+]
 
+
+def say_hello_to_pets(pets):
+    for pet in pets:
+        if pet["type"] != "dog" and pet["type"] != "cat":
+            raise Exception(pet["type"] + " is an invalid animal type")
+        print(f'{"Woof" if pet["type"] == "dog" else "Meow"}, {pet["name"]}!')
+
+
+say_hello_to_pets(animals)
