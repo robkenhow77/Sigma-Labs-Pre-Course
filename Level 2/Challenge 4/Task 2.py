b72ea0c4973bcec5b519f7c68dc2045f9f7d9c8c
robkenhow77 <102028368+robkenhow77@users.noreply.github.com>
2024-10-06 11:56:39 +0100

    Update Task 2.py

--- Level 2/Challenge 4/Task 2.py
+++ Level 2/Challenge 4/Task 2.py
@@ -1,1 +1,14 @@
+animals = [
+    {"name": "Fluffy", "type": "dog"},
+    {"name": "Parsley", "type": "dog"},
+    {"name": "Ginger", "type": "cat"},
+    {"name": "Biscuit", "type": "cat"}
+]
 
+
+def say_hello_to_pets(pets):
+    for pet in pets:
+        print(f'{"Woof" if pet["type"] == "dog" else "Meow"}, {pet["name"]}!')
+
+
+say_hello_to_pets(animals)
